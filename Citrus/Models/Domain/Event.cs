7691f591b5b9fd305621c5b840f4cdbeb1d105c6
erichdev <a.erich@gmail.com>
2015-11-07 09:38:57 -0800

    Services done

--- Citrus/Models/Domain/Event.cs
+++ Citrus/Models/Domain/Event.cs
@@ -7,6 +7,7 @@ namespace Citrus.Models.Domain
 {
     public class Event
     {
+        public int Id { get; set; }
         public string Name { get; set; }
         public string Organization { get; set; }
         public int Category { get; set; }
--- Citrus/Services/MainService.cs
+++ Citrus/Services/MainService.cs
@@ -1,5 +1,9 @@
-﻿using System;
+﻿using Citrus.Data;
+using Citrus.Models.Domain;
+using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 
@@ -7,6 +11,101 @@ namespace Citrus.Services
 {
     public class MainService : BaseService
     {
+        public static Volunteer GetVolunteerById(int Id)
+        {
+            Volunteer v = null;
 
+            DataProvider.ExecuteCmd(GetConnection, "dbo.Volunteer_SelectById"
+               , inputParamMapper: delegate (SqlParameterCollection paramCollection)
+
+               { paramCollection.AddWithValue("@Id", Id); }
+
+               , map: delegate (IDataReader reader, short set)
+               {
+                   int startingIndex = 0;
+
+                   v.Id = reader.GetSafeInt32(startingIndex++);
+                   v.Name = reader.GetSafeString(startingIndex++);
+                   v.ZipCode = reader.GetSafeString(startingIndex++);
+                   v.Email = reader.GetSafeString(startingIndex++);
+                   v.Description = reader.GetSafeString(startingIndex++);
+
+               }
+               );
+
+            return v;
+        }
+
+        public static List<Event> GetSubscribedEvents(int Id)
+        {
+            List<Event> list = new List<Event>();
+
+            DataProvider.ExecuteCmd(GetConnection, "dbo.Event_SelectByUserSubscribed"
+               , inputParamMapper: delegate (SqlParameterCollection paramCollection)
+
+               { paramCollection.AddWithValue("@Id", Id); }
+
+               , map: delegate (IDataReader reader, short set)
+               {
+                   int startingIndex = 0;
+                   Event e = new Event();
+
+                   e.Id = reader.GetSafeInt32(startingIndex++);
+                   e.Name = reader.GetSafeString(startingIndex++);
+                   e.Organization = reader.GetSafeString(startingIndex++);
+                   e.Category = reader.GetSafeInt32(startingIndex++);
+                   e.ZipCode = reader.GetSafeString(startingIndex++);
+                   e.Description = reader.GetSafeString(startingIndex++);
+
+                   if (list == null)
+                   {
+                       list = new List<Event>();
+                   }
+
+                   list.Add(e);
+
+               }
+               );
+
+            return list;
+        }
+
+
+        // ****************************
+        // **THIS CURRENTLY GETS ALL EVENTS FROM DB. WILL NEED TO UPDATE IN FRONT END TO GET EVENTS NEAR USER.
+
+        public static List<Event> GetNearbyEvents(int Id)
+        {
+            List<Event> list = new List<Event>();
+
+            DataProvider.ExecuteCmd(GetConnection, "dbo.Event_Select"
+               , inputParamMapper: delegate (SqlParameterCollection paramCollection)
+
+               {  }
+
+               , map: delegate (IDataReader reader, short set)
+               {
+                   int startingIndex = 0;
+                   Event e = new Event();
+
+                   e.Id = reader.GetSafeInt32(startingIndex++);
+                   e.Name = reader.GetSafeString(startingIndex++);
+                   e.Organization = reader.GetSafeString(startingIndex++);
+                   e.Category = reader.GetSafeInt32(startingIndex++);
+                   e.ZipCode = reader.GetSafeString(startingIndex++);
+                   e.Description = reader.GetSafeString(startingIndex++);
+
+                   if (list == null)
+                   {
+                       list = new List<Event>();
+                   }
+
+                   list.Add(e);
+
+               }
+               );
+
+            return list;
+        }
     }
 }
