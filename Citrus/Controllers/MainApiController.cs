6b82833715668a390d31f6734bd2f95a6c449ac3
Jonathan Chan <jhchan0321@gmail.com>
2015-11-07 09:51:55 -0800

    hope this resolves the error

--- Citrus/Controllers/MainApiController.cs
+++ Citrus/Controllers/MainApiController.cs
@@ -1,4 +1,6 @@
 ﻿using Citrus.Models;
+using Citrus.Models.Domain;
+using Citrus.Services;
 using System;
 using System.Collections.Generic;
 using System.Linq;
--- Citrus/Scripts/citrus.services.js
+++ Citrus/Scripts/citrus.services.js
@@ -1,1 +1,48 @@
-﻿
+﻿citrus.factories = function (apiRoot, ajaxFx) {
+
+    thisService = this;
+    thisService.getVolunteerById = _getVolunteerById;
+    thisService.getSubscribedEvents = _getSubscribedEvents;
+    thisService.getNearbyEvents = _getNearbyEvents;
+
+    function _getVolunteerById(id, onSuccess, onError) {
+        var link = apiRoot + "/volunteer/" + id
+        var settings = {
+            cache: false,
+            contentType: "application/x-www-form-urlencoded; charset=UTF-8",
+            type: 'get',
+            dataType: "json",
+            success: onSuccess,
+            error: onError
+        }
+        thisService.ajax(link, settings);
+    }
+
+    function _getSubscribedEvents(id, onSuccess, onError) {
+        var link = apiRoot + "/volunteer/" + id + "/events/subscribed"
+        var settings = {
+            cache: false,
+            contentType: "application/x-www-form-urlencoded; charset=UTF-8",
+            type: 'get',
+            dataType: "json",
+            success: onSuccess,
+            error: onError
+        }
+        thisService.ajax(link, settings);
+    }
+
+    function _getNearbyEvents(id, onSuccess, onError) {
+        var link = apiRoot + "/volunteer/" + id + "/events/nearby"
+        var settings = {
+            cache: false,
+            contentType: "application/x-www-form-urlencoded; charset=UTF-8",
+            type: 'get',
+            dataType: "json",
+            success: onSuccess,
+            error: onError
+        }
+        thisService.ajax(link, settings);
+    }
+}
+
+citrus.services = new citrus.factories('api', $.ajax);
