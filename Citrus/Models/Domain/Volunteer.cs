3123492062fcc4be3816c9eba0ef9d902cc30715
Jonathan Chan <jhchan0321@gmail.com>
2015-11-07 11:39:34 -0800

    angular script added to volunteer view

--- Citrus/Models/Domain/Volunteer.cs
+++ Citrus/Models/Domain/Volunteer.cs
@@ -9,7 +9,7 @@ namespace Citrus.Models.Domain
     {
         public int Id { get; set; }
         public string Name { get; set; }
-        public string ZipCode { get; set; }
+        public string Address { get; set; }
         public string Email { get; set; }
         public string Description { get; set; }
     }
