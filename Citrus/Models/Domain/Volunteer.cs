8c6b3e86c27747ab4f1c458e0c9934585bf9cf8a
Jonathan Chan <jhchan0321@gmail.com>
2015-11-07 07:22:27 -0800

    saturday morning push

---
+++ Citrus/Models/Domain/Volunteer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Citrus.Models.Domain
+{
+    public class Volunteer
+    {
+        public string Name { get; set; }
+        public string ZipCode { get; set; }
+        public string Email { get; set; }
+        public string Description { get; set; }
+    }
+}
